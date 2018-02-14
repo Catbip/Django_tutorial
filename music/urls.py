d9d74f117cc32d7dc2fae737eaa03da410f6e4f9
Mioara Mihai <ammihai@luxoft.com>
2018-02-14 17:47:32 +0200

    Added new functionality

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
-    # /music/<id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    # /music/<album_id>/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,24 +1,21 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from models import Album
 
 
-def index(request):
-    all_albums = Album.objects.all()
-    context = {
-        'all_albums': all_albums,
-    }
-    return render(request, 'music/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def detail(request, album_id):
-    try:
-        album = Album.objects.get(id=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist!")
+class DetailView(generic.DeleteView):
+    model = Album
+    template_name = 'music/detail.html'
 
-    context = {
-        'album': album,
-    }
 
-    return render(request, 'music/detail.html', context)
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
